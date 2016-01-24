59531d6f51e73f4cb499d3087bff6965464cecc7
Sasaki Akira <akira@Dev.local>
2016-01-24 12:36:09 +0900

    Add initial init.

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # FaBoUV-Si1132-Library
+
+
+# FaBoUV
+
+This is a library for FaBo UV Brick.
+
+# ISL29034
+
+Si1132 is SiliconLab's mems sensor for UV.
+
+# Datasheet
+
+[Si1132 Datasheet](https://www.silabs.com/Support%20Documents/TechnicalDocs/Si1132.pdf)
+
+# Releases
+
+1.0.0 Initial release.
+
+# How to install.
+
+[Installing Additional Arduino Libraries](https://www.arduino.cc/en/Guide/Libraries#toc3)
---
+++ src/fabo-si1132.cpp
@@ -0,0 +1,120 @@
+#include "fabo-si1132.h"
+
+bool si1132::searchDevice()
+{
+  byte device = 0x00;
+  readI2c(SI1132_PART_ID_REG, 1, &device);
+
+  if(device == SI1132_DEVICE){
+      return true;
+  } else{
+      return false;
+  }
+}
+
+void si1132::configuration()
+{
+  writeI2c(SI1132_UCOEF0_REG, 0x7B);
+  writeI2c(SI1132_UCOEF1_REG, 0x6B);
+  writeI2c(SI1132_UCOEF2_REG, 0x01);
+  writeI2c(SI1132_UCOEF3_REG, 0x00);
+
+  int enable_sensor = SI1132_EN_UV|SI1132_EN_AUX|SI1132_EN_ALS_IR|SI1132_EN_ALS_VIS;
+  writeI2c(SI1132_CHIPLIST_REG, enable_sensor);
+
+  // Rateの設定
+  writeI2c(Si1132_MEASRATE0_REG, 0xff);
+
+  int flag_als_encoding = SI1132_ALS_VIS_ALIGN | SI1132_ALS_IR_ALIGN;
+  writeI2c(SI1132_ALS_ENCODING_REG, flag_als_encoding);
+  
+  // Visible
+  writeI2c(SI1132_ALS_VIS_ADC_COUNTER_REG, SI1132_511_ADC_CLOCK);
+
+  writeI2c(SI1132_ALS_VIS_ADC_GAIN_REG, SI1132_1_DIVIDED_ADC_CLOCK);
+
+  writeI2c(SI1132_ALS_VIS_ADC_MISC_REG, SI1132_HIGH_SIGNAL_RANGE);
+
+  // IR
+  writeI2c(SI1132_ALS_IR_ADC_COUNTER_REG, SI1132_511_ADC_CLOCK);
+
+  writeI2c(SI1132_ALS_IR_ADC_GAIN_REG, SI1132_1_DIVIDED_ADC_CLOCK);
+
+  writeI2c(SI1132_ALS_IR_ADC_MISC_REG, SI1132_HIGH_SIGNAL_RANGE);
+
+  writeI2c(SI1132_ALS_IR_ADCMUX_REG, SI1132_ALS_IR_ADCMUX_SMALLIR);
+  // AUX
+  writeI2c(SI1132_AUX_ADCMUX_REG, SI1132_AUX_ADCMUX_TEMPERATURE);
+
+  // COMMAND
+  writeI2c(SI1132_COMMAND_REG, SI1132_COMMAND_ALS_AUTO);
+}
+
+void si1132::reset()
+{
+  writeI2c(SI1132_COMMAND_REG, SI1132_COMMAND_RESET);
+}
+
+uint16_t si1132::readUV()
+{
+  uint16_t uv_index;
+  uint8_t buffer[2];
+
+  readI2c(SI1132_AUX_DATA_REG, 2, buffer);
+  uv_index = (((uint16_t)buffer[1])<<8) | (uint16_t)buffer[0];
+
+  return uv_index;
+}
+
+uint16_t si1132::readIR()
+{
+  uint16_t ir;
+  uint8_t buffer[2];
+
+  readI2c(SI1132_IR_DATA_REG, 2, buffer);
+  ir = (((uint16_t)buffer[1])<<8) | (uint16_t)buffer[0];
+
+  return ir;
+}
+
+
+uint16_t si1132::readVisible()
+{
+  uint16_t visible;
+  uint8_t buffer[2];
+
+  readI2c(SI1132_VISIBLE_DATA_REG, 2, buffer);
+  visible = (((uint16_t)buffer[1])<<8) | (uint16_t)buffer[0];
+
+  return visible;
+}
+
+// I2Cへの書き込み
+void si1132::writeI2c(byte register_addr, byte value) {
+  Wire.beginTransmission(si1132_SLAVE_ADDRESS);  
+  Wire.write(register_addr);         
+  Wire.write(value);                 
+  Wire.endTransmission();        
+}
+
+// I2Cへの読み込み
+void si1132::readI2c(byte register_addr, int num, byte *buf) {
+  Wire.beginTransmission(si1132_SLAVE_ADDRESS); 
+  Wire.write(register_addr);           
+  Wire.endTransmission(false);         
+
+  //Wire.beginTransmission(DEVICE_ADDR); 
+  Wire.requestFrom(si1132_SLAVE_ADDRESS, num);  
+
+  int i = 0;
+  while (Wire.available())
+  {
+    buf[i] = Wire.read(); 
+    i++;   
+  }
+  //Wire.endTransmission();         
+}
+
+
+si1132 faboUV;
+
